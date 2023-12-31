c4787e69f45e30d0054411e8602dd9ec4ada99a5
Krish Agrawal <105340895+Krish3914@users.noreply.github.com>
2023-12-31 16:55:12 +0530

    Birthday Wish their their name Dynamically.

---
+++ asd.js
@@ -0,0 +1,15 @@
+// function submitForm()
+// {
+
+// var b = document.getElementById('username').value
+
+// document.getElementById('here').innerHTML = b;
+
+// }
+function submitForm(){
+        let user = document.getElementById('username').value;
+        console.log(user);
+    // let user = username.value;
+    // localStorage.setItem('username', user);
+        // console.log(user);
+    }
